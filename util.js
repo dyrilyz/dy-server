58f593db3fc6820d9f0081690b44fb5667486d41
zhanghaoxiang <zhanghaoxiang@ludashi.com>
2019-06-11 02:33:06 +0800

    my-server

---
+++ util.js
@@ -0,0 +1,38 @@
+const fs = require('fs')
+const cheerio = require('cheerio')
+
+module.exports = {
+    readFile (path) {
+        return new Promise((resolve, reject) => {
+            fs.readFile(path, (e, data) => {
+                if (e) {
+                    reject(e)
+                } else {
+                    resolve(data)
+                }
+            })
+        })
+    },
+    readdir (path) {
+        return new Promise((resolve, reject) => {
+            fs.readdir(path, (e, data) => {
+                if (e) {
+                    reject(e)
+                } else {
+                    resolve(data)
+                }
+            })
+        })
+    },
+    jq (path) {
+        return this.readFile(path).then(data => {
+            return cheerio.load(data.toString())
+        })
+    },
+    isFile (path) {
+        return fs.statSync(path).isFile()
+    },
+    isDir (path) {
+        return !this.isFile(path)
+    }
+}
